25ee1f374dd98f277fdc236c5ae500add9e1f3bb
Bruno Correr <bruno.correr.coa@gmail.com>
2025-04-25 11:38:21 -0300

    adicionando exercícios

---
+++ BASICO/desafio2.js
@@ -0,0 +1,4 @@
+let raio = 10;
+let pi = 3.14159;
+let circ = pi * (raio * raio)
+console.log(circ)
---
+++ BASICO/ex002.js
@@ -0,0 +1,4 @@
+var hhh = 7666.989;
+console.log(typeof(hhh));
+var sim = true;
+console.log(typeof(sim));
