2a70e96e85103a1330710e0c593ff885b60001ef
Bruno Correr <bruno.correr.coa@gmail.com>
2025-04-28 11:45:43 -0300

    Criando exercícios

---
+++ FUNDAMENTOS_JS/string.js
@@ -0,0 +1,9 @@
+const escola = "Escola"
+console.log(escola.charAt(3))
+console.log(escola.charAt(4)) //Mostra o char, na posição que você escolher
+console.log(escola.indexOf("l")) //Mostra que posição está o que você incluiu
+console.log(escola.substring(1))
+console.log(escola.substring(3, 5)) //Imprime do indice três, até o quarto e corta o 5
+console.log("Escola:" .concat(escola).concat("cu"))
+console.log(escola.replace(2, 'e'))
+
---
+++ FUNDAMENTOS_JS/tiposdedados.js
@@ -0,0 +1,14 @@
+const n1 = 1.0;
+const n2 = Number("9.8")
+
+console.log(Number.isInteger(n1))
+console.log(Number.isInteger(n2))
+
+const n3 = 3.14987678453;
+console.log(n3.toFixed(2)) //Exibe somente dois números depois da virgula
+console.log("Num 3:")
+console.log(typeof(n3))
+console.log("Num 2:")
+console.log(typeof(n2))
+console.log("Num 1:")
+console.log(typeof(n1))
