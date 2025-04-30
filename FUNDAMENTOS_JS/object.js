a479b913256a317989011198022c8bda0081e0cc
Bruno Correr <bruno.correr.coa@gmail.com>
2025-04-30 08:59:51 -0300

    Objeto criado

---
+++ FUNDAMENTOS_JS/object.js
@@ -0,0 +1,13 @@
+obj = {} //Criando um objeto
+obj.idade = 17; //Criando uma "variável idade e adicionando um valor"
+obj.nome = "Bruno"
+obj.nacionalidade = "Brasileiro"
+
+obj2 = {
+    nome: "BrunoC",
+    idade : 17,
+    nacionalidade: Italiano
+}
+
+console.log(obj)
+console.log(obj2)
